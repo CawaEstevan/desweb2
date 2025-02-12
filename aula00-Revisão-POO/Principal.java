06cdf47b871a27873a6e5b705e38ad6392c2a4d1
CawaEstevan <145603770+CawaEstevan@users.noreply.github.com>
2025-02-12 22:00:20 +0000

    Lista disciplina concluido

--- aula00-Revisão-POO/Principal.java
+++ aula00-Revisão-POO/Principal.java
@@ -3,7 +3,10 @@ public class Principal{
     public static void main(String[] args) {
 
         Aluno aluno01 = new Aluno("Cawa",35,215551652,1004);
-        System.out.println(aluno01.toString()); 
+        System.out.println(aluno01.toString());
+
+        Aluno aluno02 = new Aluno("Carlos",30,2135651652,1204);
+        System.out.println(aluno02.toString());
 
         Professor professor1 = new Professor ("Matheus",26,525525252,25);
         System.out.println(professor1.toString());
@@ -11,8 +14,24 @@ public class Principal{
         Professor professor2 = new Professor ("Math",23,52525252,23);
         System.out.println(professor2.toString());
 
+
         Disciplina disciplina = new Disciplina ("Web 2");
-        System.out.println(disciplina.toString());
+
+         // Adicionando alunos e professores à disciplina
+         disciplina.addAluno(aluno01);
+         disciplina.addProfessor(professor1);
+         disciplina.addProfessor(professor2);
+         
+         // Imprimindo a disciplina com os alunos e professores adicionados
+         System.out.println(disciplina.toString());
+        
+
+
+
     }
+   
+
+
+
     
 }
