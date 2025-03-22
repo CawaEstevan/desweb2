469f8be2c55a70fd4a0860c556d2ef24c8114c86
CawaEstevan <145603770+CawaEstevan@users.noreply.github.com>
2025-03-22 12:21:49 +0000

    NEW

--- Cawa_Lista1/EX1/ex1/src/main/java/aplicativo/Principal.java
+++ Cawa_Lista1/EX1/ex1/src/main/java/aplicativo/Principal.java
@@ -1,54 +1,43 @@
 package aplicativo;
+
 import dominio.Pessoa;
-import dominio.Professor;
 import dominio.Vendedor;
-import dominio.Aluno;
+import dominio.Cliente;
 import java.util.ArrayList;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+
 public class Principal {
-public static void main(String[] args) {
-//Instancia o EntityManagerFactory com as configurações de persistencia
-EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula-jpa");
-//Intancia o EntityManager
-EntityManager em = emf.createEntityManager();
-
-Pessoa pessoa1 = new Pessoa("Rafael", 35);
-Vendedor vendedor1 = new Vendedor("Cawa", 50, 01);
-Cliente cliente1 = new Cliente("Gabriel", 20, 01);
-
-
-em.getTransaction().begin();// iniciar transação com banco de dados
-
-em.persist(pessoa1);
-em.persist(vendedor1);
-em.persist(cliente1);
-
-
-//consulta em jpql
-Query consultaP = em.createQuery("select pessoa1 from Pessoa pessoa1");
-ArrayList<Pessoa> listaP = (ArrayList<Pessoa>) consultaP.getResultList();
-
-//consulta em jpql
-Query consultaA = em.createQuery("select vendedor1 from Vendedor vendedor");
-ArrayList<Vendedor> listaA = (ArrayList<Vendedor>) consultaA.getResultList();
-
-Query consultaC = em.createQuery("select cliente1 from Cliente cliente1");
-ArrayList<Cliente> listaC = (ArrayList<Cliente>) consultaA.getResultList();
-
-em.getTransaction().commit();//confirmar as alterações realizadas
-emf.close();
-em.close();
-for(Pessoa objP: listaP) {
-System.out.println(objP);
-}
-for(Vendedor objA: listaA) {
-System.out.println(objA);
-}
-for(Cliente objC: listaA) {
-    System.out.println(objC);
+    public static void main(String[] args) {
+       
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ex1-jpa");
+        
+        EntityManager em = emf.createEntityManager();
+        
+        // Criar instâncias conforme solicitado na atividade
+        Pessoa pessoa = new Pessoa("José", 35);
+        Vendedor vendedor = new Vendedor("Cawa", 20, 1); 
+        Cliente cliente = new Cliente("Rafael", 34, 1);
+        
+        em.getTransaction().begin(); // iniciar transação com banco de dados
+        em.persist(pessoa);
+        em.persist(vendedor);
+        em.persist(cliente);
+        
+        // Consulta para recuperar todas as pessoas
+        Query consultaP = em.createQuery("select p from Pessoa p");
+        ArrayList<Pessoa> listaP = (ArrayList<Pessoa>) consultaP.getResultList();
+        
+        em.getTransaction().commit(); // confirmar as alterações realizadas
+        
+        emf.close();
+        em.close();
+        
+        // Exibindo resultados
+        for (Pessoa p: listaP) {
+            System.out.println(p);
+        }
     }
-}
 }
