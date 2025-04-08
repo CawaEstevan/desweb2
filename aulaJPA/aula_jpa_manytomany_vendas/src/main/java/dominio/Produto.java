360cff4c055e5752e89060527c7c9461ecfc489b
CawaEstevan <145603770+CawaEstevan@users.noreply.github.com>
2025-04-08 17:21:35 +0000

    ok

--- aulaJPA/aula_jpa_manytomany_vendas/src/main/java/dominio/Produto.java
+++ aulaJPA/aula_jpa_manytomany_vendas/src/main/java/dominio/Produto.java
@@ -2,16 +2,15 @@ package dominio;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 
 @Entity
-public class Cliente {
+public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,15 +18,19 @@ public class Cliente {
     @Column(name = "nome")
     private String nome;
     
-    @OneToMany(mappedBy = "cliente")
+    @Column(name = "preco")
+    private Double preco;
+    
+    @ManyToMany(mappedBy = "produtos")
     private List<Venda> vendas = new ArrayList<>();
     
-    public Cliente() {
-        this("");
+    public Produto() {
+        this("", 0.0);
     }
     
-    public Cliente(String nome) {
+    public Produto(String nome, Double preco) {
         setNome(nome);
+        setPreco(preco);
     }
     
     public Long getId() {
@@ -46,6 +49,14 @@ public class Cliente {
         this.nome = nome;
     }
     
+    public Double getPreco() {
+        return preco;
+    }
+    
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    
     public List<Venda> getVendas() {
         return vendas;
     }
@@ -56,6 +67,6 @@ public class Cliente {
     
     @Override
     public String toString() {
-        return "Cliente [id=" + id + ", nome=" + nome + "]";
+        return "Produto [id=" + id + ", nome=" + nome + ", preco=" + preco + "]";
     }
 }
